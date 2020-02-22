0756fa66104d864b64681778d4ccb38a2e000735
Jossi Muñoz <jossimunoz@MacBook-Pro-de-Jossi.local>
2020-02-21 22:10:12 -0300

    adding profit calculator

--- calculator.py
+++ calculator.py
@@ -14,6 +14,7 @@ if choice in currency_pair:
 
     print("0. Buy (market price)")
     print("1. Sell (market price)")
+    print("2. Buy and Sell (Profits)")
     
     operation = float(input(bcolors.OKGREEN + "Operation you want to do? [0]: " + bcolors.ENDC) or 0)
 
@@ -23,11 +24,19 @@ if choice in currency_pair:
 
         currency_pair = [choice[0:3].upper(), choice[3:6].upper()]
 
+        # the amount we want to buy
+
         amount = float(input("Enter Amount (" + currency_pair[1] + "): "))
+
+         # requesting api with our selected currency pair
 
         currency_pair_amount = api.getJSON(choice)
 
+        # detecting if the api responded well to our request
+
         if currency_pair_amount:
+
+            # print transaction
 
             print(bcolors.OKGREEN + "PURCHASE PRICE: " + currency_pair_amount['ask'] + bcolors.ENDC)
             print(bcolors.OKGREEN + "TRANSACTION FEE: " + str(transaction_fee) + bcolors.ENDC)
@@ -36,7 +45,41 @@ if choice in currency_pair:
         else:
 
             print(bcolors.FAIL + "Bitstamp API Error" + bcolors.ENDC)
-            
+
+    elif operation == 2:
+
+        # getting the currency separately Ex. BTCUSD > ['BTC', 'USD']
+
+        currency_pair = [choice[0:3].upper(), choice[3:6].upper()]
+
+        # amount to be invested
+
+        your_investment = float(input("Your money to invest (" + currency_pair[1] + "): "))
+
+        # purchase price
+        
+        currency_value_buy = input(choice.upper() + " Value (Buy): ") 
+
+        # calculating purchase occupying the buy function
+
+        calculating_purchase = buy(your_investment, float(currency_value_buy))
+
+        # printing the purchase
+
+        print(bcolors.OKGREEN + "You have now: " + currency_pair[0] + " " + calculating_purchase + bcolors.ENDC)
+
+        currency_value_sell = input(choice.upper() + " Value (Sell): ")
+
+        calculating_sale = sell(float(calculating_purchase), float(currency_value_sell))
+
+
+        print(bcolors.OKGREEN + "You sold: " + currency_pair[0] + " " + calculating_purchase + bcolors.ENDC)
+        print(bcolors.OKGREEN + "You get: " + currency_pair[1] + " " + calculating_sale + bcolors.ENDC)
+
+        profit = float(calculating_sale) - float(your_investment)
+
+        print(bcolors.OKGREEN + "You profit: " + currency_pair[1] + " " + str(profit) + bcolors.ENDC)
+        
 
 
     else:
